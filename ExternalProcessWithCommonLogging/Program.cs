4c05d64e746f1f50b5dcb10d3a03cd727068a43b
Kjell Otto <otto.kjell@gmail.com>
2013-11-10 13:37:09 +0100

    - initial commit, got external process to attach to

---
+++ ExternalProcessWithCommonLogging/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading;
+using Common.Logging;
+
+namespace ExternalProcessWithCommonLogging
+{
+    class Program
+    {
+        static void Main()
+        {
+            var thread = new Thread(() =>
+            {
+                var a = new ALogged();
+
+                try
+                {
+
+                    while (true)
+                    {
+                        a.AMethodDebug();
+                        a.AMethodInfo();
+                        a.AMethodWarn();
+                        a.AMethodError();
+                        Console.WriteLine("Logged something with Common.Logging.");
+                        Thread.Sleep(1000);
+                    }
+                }
+                catch (ThreadInterruptedException)
+                {
+                    Console.WriteLine("Stopping Debugging Thread.");
+                }
+                // ReSharper disable once FunctionNeverReturns
+            });
+            thread.Start();
+
+            Console.WriteLine("Press <Enter> to exit...");
+            Console.ReadLine();
+
+            thread.Interrupt();
+            thread.Join();
+        }
+    }
+
+    class ALogged
+    {
+        private ILog _log = LogManager.GetCurrentClassLogger();
+
+        internal void AMethodDebug()
+        {
+            _log.Debug("AMethodDebug()");
+        }
+
+        internal void AMethodInfo()
+        {
+            _log.Info("AMethodInfo()");
+        }
+
+        internal void AMethodWarn()
+        {
+            _log.Warn("AMethodWarn()");
+        }
+
+        internal void AMethodError()
+        {
+            _log.Error("AMethodError()");
+        }
+    }
+
+}
